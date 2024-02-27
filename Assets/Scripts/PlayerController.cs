6701db54d05aa8f23776f2ea2f5fd130b960b083
dixtar321 <pogosoffal@yandex.ru>
2024-02-27 15:42:49 +0300

    Player control Added borders: player can't go out of game scene. Made code cleaner (add variables)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float _horizontalInput;
-    private float _playerSpeed = 15.0f;
+    private float _playerSpeed = 25.0f;
+    private float _xRange = 20.0f;
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -_xRange)
+        {
+            transform.position = new Vector3(-_xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > _xRange)
+        {
+            transform.position = new Vector3(_xRange, transform.position.y, transform.position.z);
+        }
         _horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * _horizontalInput * _playerSpeed * Time.deltaTime);
     }
