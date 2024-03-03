b6167b439180f0475d891bdf823669d8898dc8d7
dixtar321 <pogosoffal@yandex.ru>
2024-03-03 11:31:42 +0300

    Added DetectCollisions Added Challenge2

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,12 +10,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if an object past the player veiw in the game -> remove that object
         if(transform.position.z > _topBorder)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < _lowBorder)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
-    private float spawnPositionZ = 20;
+    private float spawnPositionZ = 25;
+    private float startDelay = 2;
+    private float startInterval = 1.0f;
 
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, startInterval);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnCords = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnCords, animalPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnCords = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnCords, animalPrefabs[animalIndex].transform.rotation);
     }
 }
